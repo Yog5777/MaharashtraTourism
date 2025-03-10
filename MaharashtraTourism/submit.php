62724c6cebfd194129e5c1fda8329ac4c0c95e28
Yogesh <yogeshchandak08@gmail.com.com>
2025-03-10 19:48:02 +0530

    MaharashtraTourism

---
+++ MaharashtraTourism/submit.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_Post['btn-save']))
+{
+    echo'workig';
+}
+//$firstName= $_POST['fname'];
+//$address= $_POST['addrs'];
+//$mobile= $_POST['mobnum'];
+//$email= $_POST['email'];
+    
+
+// Create connection
+//$conn = new mysqli('localhost','root','','tbl_student');
+// Check connection
+//if ($conn->connect_error) {
+  //die('Connection failed: ' . $conn->connect_error);
+//}else{
+    //$stmt=$conn->prepare("insert into tbl_student(st_name,st_add,st_mob,email)values
+    //(?,?,?,?)");
+
+   // $stmt->bind_param("ssis",$firstName, $address, $mobile, $email);
+   // $stmt->execute();
+
+  //echo "New record created successfully";
+//$stmt->close();
+//$conn->close();
+//}
+?>
